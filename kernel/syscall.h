d5adeb50da1e953c59f8240a438be2b5a7b53b10
unkn0va <lyunkn0vaa@gmail.com>
2025-09-29 19:29:49 +0900

    Update syscall.h

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -20,3 +20,9 @@
 #define SYS_link   19
 #define SYS_mkdir  20
 #define SYS_close  21
+#define SYS_getpname 22
+#define SYS_getnice 23
+#define SYS_setnice 24
+#define SYS_ps 25
+#define SYS_meminfo 26
+#define SYS_waitpid 27
