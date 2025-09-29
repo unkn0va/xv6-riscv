5068adf32e55abfa8f209c99ff7c5acff56b2b7d
unkn0va <lyunkn0vaa@gmail.com>
2025-09-29 19:43:47 +0900

    Update proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -92,6 +92,8 @@ struct proc {
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
 
+  int nice;
+
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
 
