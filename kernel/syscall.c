752f3b3b02cc0b7d13828ccc20e26991edea5666
unkn0va <lyunkn0vaa@gmail.com>
2025-09-29 19:31:25 +0900

    Update syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -101,6 +101,12 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_getpname(void);
+extern uint64 sys_getnice(void);
+extern uint64 sys_setnice(void);
+extern uint64 sys_ps(void);
+extern uint64 sys_meminfo(void);
+extern uint64 sys_waitpid(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -126,6 +132,12 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_getpname] sys_getpname,
+[SYS_getnice] sys_getnice,
+[SYS_setnice] sys_setnice,
+[SYS_ps] sys_ps,
+[SYS_meminfo] sys_meminfo,
+[SYS_waitpid] sys_waitpid,
 };
 
 void
