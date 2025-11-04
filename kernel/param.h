4a204a34a60395660c3aad00e9870106b1d67b0f
unkn0va <lyunkn0vaa@gmail.com>
2025-11-04 23:11:28 +0900

    Update param.h

--- kernel/param.h
+++ kernel/param.h
@@ -13,3 +13,8 @@
 #define MAXPATH      128   // maximum file path name
 #define USERSTACK    1     // user stack pages
 
+#define PROT_READ 0x1
+#define PROT_WRITE 0x2
+
+#define MAP_ANONYMOUS 0x1
+#define MAP_POPULATE 0x2
