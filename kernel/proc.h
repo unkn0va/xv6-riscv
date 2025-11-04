297a0a19f2ea6d8060216c846ed08430c329aafd
unkn0va <lyunkn0vaa@gmail.com>
2025-11-04 23:37:16 +0900

    Update proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -115,3 +115,13 @@ struct proc {
 };
 
 extern const int weights[40];
+
+struct mmap_area {
+  struct file *f;
+  uint64 addr;
+  int length;
+  int offset;
+  int prot;
+  int flags;
+  struct proc *p;
+}
