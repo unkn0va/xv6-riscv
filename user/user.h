51734282ecf044b513c4d271c86cfb99ebdc2628
unkn0va <lyunkn0vaa@gmail.com>
2025-11-05 11:00:42 +0900

    Update user.h

--- user/user.h
+++ user/user.h
@@ -31,6 +31,10 @@ void ps(int);
 int meminfo(void);
 int waitpid(int);
 
+uint64 mmap(uint64 addr, int length, int prot, int flags, int fd, int offset);
+int munmap(uint64 addr);
+int freeemem(void);
+
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
